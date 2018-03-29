d9178b5c3a0a65591becffd646b73513aceeea38
Hitesh Jasani <hitesh.jasani@gmail.com>
2018-03-29 08:16:24 -0400

    0.1.1-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject jasani-serverless-cljs/lein-template "0.1.0"
+(defproject jasani-serverless-cljs/lein-template "0.1.1-SNAPSHOT"
   :description "A leiningen template to generate a serverless cljs/clojurescript project"
   :url "https://github.com/hiteshjasani/jasani-serverless-cljs-template"
   :license {:name "Eclipse Public License"
